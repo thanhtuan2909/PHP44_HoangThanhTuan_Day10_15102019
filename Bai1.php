106f3351c5e356131bbbc13ff691896372804fca
thanhtuan2909 <tuandhv96@gmail.com>
2019-10-16 21:02:47 +0700

    Bài 1 Day 10

---
+++ Bai1.php
@@ -0,0 +1,26 @@
+<?php
+$variable1 = "1.23";
+$variable2 = 28;
+$variable3 = "null";
+$variable4 = [123,false,null,1.23,FALSE,[],TRUE];
+$variable5 = (float) -123;
+$variable6 = "false";
+$variable7 = "php39";
+
+
+
+var_dump($variable1);
+echo "<br>";
+var_dump($variable2);
+echo "<br>";
+var_dump($variable3);
+echo "<br>";
+echo "<pre>";
+var_dump($variable4);
+echo "</pre>";
+echo "<br>";
+var_dump($variable5);
+echo "<br>";
+var_dump($variable6);
+echo "<br>";
+var_dump($variable7);
